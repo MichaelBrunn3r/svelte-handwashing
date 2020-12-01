6cf2538cffc7bf83c11b74570822e9440dae5cd3
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-01 11:05:16 +0100

    adding rough html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,20 @@
+<script>
+	import Timer from './Timer.svelte';
+	import HowTo from './HowTo.svelte';
+</script>
+
+<style>
+	h1,h3 {
+		text-align: center;
+	}
+</style>
+
 <h1>Test</h1>
+<Timer/>
+<HowTo/>
+
+
+<h3>
+	<a href="https://www.who.int/gpsc/clean_hands_protection/">Picture source</a>
+	<a href="https://freesound.org/people/metrostock99/sounds/345086">Sound source</a>
+</h3>
---
+++ src/HowTo.svelte
@@ -0,0 +1,9 @@
+<style>
+	img {
+		max-width: 100%;
+	}
+</style>
+
+<div bp="grid">
+	<img bp="offset-5@md 4@md 12@sm" src="washing_guide.png" alt="Guide on how to wash your hands">
+</div>
